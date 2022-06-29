bc86036ba127471874e531d10d383e03cda1e6cb
Jacob Menachem <dorlabor1@gmail.com>
2022-06-29 15:52:25 +0300

    feat: scrolled navbar, links to keep and email

--- js/cmp/app-header.cmp.js
+++ js/cmp/app-header.cmp.js
@@ -1,22 +1,56 @@
 export default {
   template: `
-  <header :class="{'scrolled-nav':scrollPosition}">
+  <header :class="{'scrolled-nav':scrollNav}">
     <nav>
       <div class="branding">
-      <img alt="logo">
-      <ul v-show="!mobile" class="navigation">
-        <li><router-link class="link" to="">Books</router-link></li>
-        <li><router-link class="link" to="/keep">Keep</router-link></li>
-        <li><router-link class="link" to="/email">Email</router-link></li>
-      </ul>
+      <router-link class="link" to="/"><img src="/img/Appsus.png" alt="logo"></router-link>
       </div>
+      <ul v-show="!mobile" class="navigation">
+        <li><router-link class="link" to="">📚Books</router-link></li>
+        <li><router-link class="link" to="/keep">📌Keep</router-link></li>
+        <li><router-link class="link" to="/email">📧Email</router-link></li>
+      </ul>
+      <div class="icon">
+        <span @click="toogleMobileNav" v-show="mobile" :class="{'icon-active' : mobileNav}">☰</span>
+      </div>
+      <transition name="mobile-nav">
+      <ul v-show="mobileNav" class="dropdown-nav">
+        <li><router-link class="link" to="">📚Books</router-link></li>
+        <li><router-link class="link" to="/keep">📌Keep</router-link></li>
+        <li><router-link class="link" to="/email">📧Email</router-link></li>
+      </ul>
+      </transition>
     </nav>
   </header>
    `,
+  name: 'navigation',
   data() {
-    return {}
+    return {
+      scrollNav: null,
+      mobile: null,
+      mobileNav: null,
+      windowWidth: null,
+    }
   },
-  methods: {},
+  created() {
+    window.addEventListener('resize', this.cheackScreen)
+    this.cheackScreen()
+  },
+  methods: {
+    toogleMobileNav() {
+      this.mobileNav = !this.mobileNav
+    },
+    cheackScreen() {
+      this.windowWidth = window.innerWidth
+      if (this.windowWidth <= 750) {
+        this.mobile = true
+        return
+      }
+      this.mobile = false
+      this.mobileNav = false
+      return
+    },
+  },
   computed: {},
 }
 /* <router-link to="/">Home</router-link>|
